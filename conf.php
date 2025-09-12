45a17b652518ae014bc5b5b923436eb668d798f6
Jason <jason.kamwaro@strathmore.edu>
2025-09-12 04:52:23 +0300

    Removed Passwords

--- conf.php
+++ conf.php
@@ -3,13 +3,13 @@
 $conf['db_type'] = 'pdo';
 $conf['db_host'] = 'localhost';
 $conf['db_user'] = 'root';
-$conf['db_pass'] = 'Wachira2005';
+$conf['db_pass'] = '';
 $conf['db_name'] = 'testdb';
 
 // Email configuration
 $conf['mail_type'] = 'smtp'; 
 $conf['smtp_host'] = 'smtp.gmail.com';
 $conf['smtp_user'] = 'jason.kamwaro@strathmore.edu';
-$conf['smtp_pass'] = 'eoqb hydd nnnm idqw';
+$conf['smtp_pass'] = '';
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
